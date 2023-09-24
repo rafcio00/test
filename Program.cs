9c0e0e7df50faa9a36774fc70e12d5f0d17092db
rafcio00 <145892983+rafcio00@users.noreply.github.com>
2023-09-24 08:46:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Person
+    {
+        public string Name;
+        public string LastName;
+        public string Country;
+        public string Street;
+        public int Age;
+
+        public Person(string name, string lastName, string country, string street, int age)
+        {
+            Name = name;    
+            LastName = lastName;
+            Country = country;
+            Street = street;
+            Age = age;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person rafal = new Person("Rafal", "Zuzewicz", "Sweden", "Grönbacka", 26);
+
+            Console.WriteLine(rafal.Name);
+
+
+
+            string[] dane = { "Rafal", "Zuzewicz", "Poland", "Grönbacka ", "26" };
+            
+            for(int i = 0; i < dane.Length; i++)
+            {
+                Console.WriteLine(dane[i]);
+            }
+        }
+    }
+}
